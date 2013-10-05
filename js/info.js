54916669384315f6476bc45defc2a01a6a69e87c
moyicat <moyicat@gmail.com>
2013-10-05 17:21:45 -0500

    Update Info Basic

--- js/info.js
+++ js/info.js
@@ -1,12 +1,21 @@
 function update(cSpeed, cTime, tTime, gTime) {
 	info.parent().toggle().siblings().toggle();
 	cTime = new Date();
-	tTime = new Date("2013-10-05T22:21:45.504Z");
+	tTime = new Date("2013-10-04T23:21:45.504Z");
 	gTime = 1000;
 	var rTime = tTime.getMilliseconds() - cTime.getMilliseconds();
-	if (gTime > rTime){
-		info.empty().append("Relax!");
+	var i=0;
+	console.log(rTime);
+	if (gTime + 90 > rTime){
+		i = Math.round((gTime - rTime)/60);
+		info.parent().parent().addClass('relax');
+		info.empty().append("Relax! You will arrive at your location " + i + " mins earlier :)");
+	} else if (gTime - 90 < rTime) {
+		i = Math.round((rTime - gTime)/60);
+		info.parent().addClass('hurry');
+		info.empty().append("Hurry! You will arrive at your destiny " + i + " mins late!")
 	} else {
-		info.empty().append("Hurry!")
+		info.parent().addClass('ontime');
+		info.empty().append("Just keep up, you will be on time :)")
 	}
 }
