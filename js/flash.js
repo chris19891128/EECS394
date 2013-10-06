a729fbe164a51a8653536d5f194f8a4d8561602d
moyicat <moyicat@gmail.com>
2013-10-05 20:51:48 -0500

    Flash Added

---
+++ js/flash.js
@@ -0,0 +1,32 @@
+var f; //SetTimeout
+
+function flash(conf) {
+	if (conf == "slow") {
+//		navigator.vibrate(500);
+		info.parent().parent().animate({opacity: 0},150,function(){
+			info.parent().parent().animate({opacity: 1}, 150, function(){
+				var f = setTimeout(function(){
+					flash("slow");
+				},1200);
+			});
+		});
+	} else if (conf == "medium") {
+//		navigator.vibrate(200);
+		info.parent().parent().animate({opacity: 0},100,function(){
+			info.parent().parent().animate({opacity: 1}, 100, function(){
+				var f = setTimeout(function(){
+					flash("medium");
+				},800);
+			});
+		});
+	} else {
+//		navigator.vibrate(200);
+		info.parent().parent().animate({opacity: 0},80,function(){
+			info.parent().parent().animate({opacity: 1}, 80, function(){
+				var f = setTimeout(function(){
+					flash("fast");
+				},300);
+			});
+		});		
+	}
+}
--- js/info.js
+++ js/info.js
@@ -4,14 +4,20 @@ function update(cSpeed, cTime, tTime, gTime) {
 	console.log(rTime);
 	console.log(gTime);
 	if (gTime + 90 < rTime){
+		clearTimeout(f);
+		flash('slow');
 		i = Math.round((rTime - gTime)/60);
 		info.parent().parent().removeAttr('class').addClass('relax');
 		info.empty().append("Relax! You will arrive at your location " + i + " mins earlier :)");
 	} else if (gTime - 90 > rTime) {
+		clearTimeout(f);
+		flash('fast');
 		i = Math.round((gTime - rTime)/60);
 		info.parent().parent().removeAttr('class').addClass('hurry');
 		info.empty().append("Hurry! You will arrive at your destination " + i + " mins late!")
 	} else {
+		clearTimeout(f);
+		flash('medium');
 		info.parent().parent().removeAttr('class').addClass('ontime');
 		info.empty().append("Just keep up, you will be on time :)")
 	}
