6a844514fb36d8d296d98ecb4befefe77ca8e04a
chris19891128 <shichao@SHICHAOs-MacBook-Pro.local>
2013-10-20 16:26:54 -0500

    user login page test

---
+++ js/parse/homepage.js
@@ -0,0 +1,40 @@
+
+function currentUser() {
+	var currentUser = Parse.User.current();
+	if (currentUser) {
+		alert("login already with email " + currentUser.email);
+	} else {
+		alert("You are not log in");
+	}
+}
+
+function signUp() {
+	var user = new Parse.User();
+	user.set("username", document.getElementById("usrname").value);
+	user.set("password", document.getElementById("pwd").value);
+	user
+			.set("email", document.getElementById("usrname").value
+					+ "@example.com");
+
+	user.signUp(null, {
+		success : function(user) {
+			// Hooray! Let them use the app now.
+		},
+		error : function(user, error) {
+			// Show the error message somewhere and let the user try again.
+			alert("Error: " + error.code + " " + error.message);
+		}
+	});
+}
+
+function logIn() {
+	Parse.User.logIn(document.getElementById("usrname").value, document
+			.getElementById("pwd").value, {
+		success : function(user) {
+			alert("login success with email " + user.email);
+		},
+		error : function(user, error) {
+			// The login failed. Check error to see why.
+		}
+	});
+}
